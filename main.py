ae6187259167b35758ece491b59a910b10b212c0
elechen <elechen@outlook.com>
2016-03-26 22:32:15 +0800

    3

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ app.secret_key = "YL_DIANCAN_20160326_SECRET_KEY"
 @app.route('/')
 def index():
 	check_auto_login()
-	resp = make_response(render_template('index.html'))
-	resp.set_cookie("lang", "CN")
-	print(request, "==================")
-	print(request.cookies, "==================")
-	return resp
+	if session.get('logged_in'):
+		return redirect(url_for('home'))
+	else:
+		return render_template('index.html')
 
 def check_auto_login():
 	username = request.cookies.get('username')
@@ -29,35 +28,38 @@ def is_valid_user(name, pwd):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	error = None
+	if session.get("logged_in"):
+		return redirect(url_for('home'))
 	if request.method == 'POST':
 		username, password = request.form['username'], request.form['password']
 		if is_valid_user(username, password):
-			resp = make_response(render_template('index.html'))
-			resp.set_cookie('username', value=username)
-			resp.set_cookie('password', value=password)
+			resp = make_response(redirect(url_for('index')))
+			resp.set_cookie('username', username, max_age=365*24*60*60)
+			resp.set_cookie('password', password, max_age=365*24*60*60)
 			session['logged_in'] = True
 			session['username'] = username
-			return redirect(url_for('home'))
+			return resp
 		else:
 			error = 1
-			session['logged_in'] = False
 
 	return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
 	session.pop('logged_in', None)
+	session.pop('username', None)
 	flash('You were logged out')
-	return redirect(url_for('index'))
-
-def on_login_done(name):
-	session['logged_in'] = True
-	session['username'] = name
-	return redirect(url_for('home'))
+	resp = make_response(redirect(url_for('index')))
+	resp.delete_cookie('username')
+	resp.delete_cookie('password')
+	return resp
 
 @app.route('/home')
 def home():
-	return 'welcome to DianCan, %s' % (session['username'])
+	if session.get('logged_in'):
+		return '你好, %s' % (session['username'])
+	else:
+		return redirect(url_for('login'))
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', debug=False)
